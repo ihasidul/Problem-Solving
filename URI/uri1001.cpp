909f01102c3e95c25e993f5f8e81f302cd059c66
ihasidul <ihasidul@gmail.com>
2020-07-03 18:29:44 +0600

    Accepted

---
+++ URI/uri1001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+    cin >> x;
+    cin >> y;
+    int sum = x + y;
+    cout << "X = " << sum << endl;
+
+    return 0;
+}
