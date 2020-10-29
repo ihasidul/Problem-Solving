746a9d58cf60911ec5abdf3cb112e12d0e64b3e2
ihasidul <ihasidul@gmail.com>
2020-10-29 22:32:08 +0600

    Reverse string accepted

---
+++ leetcode/344_Reverse_String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for item in range(len(s)//2):
+            temp = s[item]
+            s[item] = s[len(s) - (1 + item)]
+            s[len(s) - (1 + item)] = temp
+            #print(s)
+            
