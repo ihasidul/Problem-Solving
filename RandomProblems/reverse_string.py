f9888118a9df40369f88ef84f8f2c31e91596553
ihasidul <ihasidul@gmail.com>
2021-05-27 10:01:19 +0600

    add new way of reverse string but it does not workds

--- RandomProblems/reverse_string.py
+++ RandomProblems/reverse_string.py
@@ -1,5 +1,13 @@
 #Write a function that will receive a string as an input and will return the string in reverse order
 def reverse_string(s):
     return s [::-1]
+def another_reverse_string(s):
+    s_len = len(s)
+    result = ""
+    while(s_len >= 0):
+        result += s[s_len]
+        s_len -= 1
 
-print(reverse_string("Hekko"))
+
+    return result
+print(another_reverse_string("Hekko"))
