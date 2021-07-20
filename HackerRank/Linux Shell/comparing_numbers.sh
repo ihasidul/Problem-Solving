6b1d49486a978685d6f91a66d4d818cef2dbeba5
ihasidul <ihasidul@gmail.com>
2021-07-20 13:36:23 +0600

    Add Comparing numbers problem

---
+++ HackerRank/Linux Shell/comparing_numbers.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+read X
+read Y
+if (( X > Y )); then
+    printf "X is greater than Y"
+elif (( X == Y )); then
+    printf "X is equal to Y"
+else
+    printf "X is less than Y"
+fi
+
+
