ed0db166b238414674970f77519091216af68af2
ihasidul <ihasidul@gmail.com>
2021-09-25 00:46:57 +0600

    Add test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pointer = 0
+        pointer2 = 1
+        
+        for i in range(len(nums)-1):
+            if nums[i] != nums[i + 1]:
+                pointer += 1
+                nums[pointer] = nums[i + 1]
+        print(nums)
+        print(pointer)
+        for i in range(pointer,len(nums)-1):
+            nums[i + 1] = '_'
+        print(nums)
