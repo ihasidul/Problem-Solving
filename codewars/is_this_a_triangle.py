836f5926ef50603876cc2dc5d45d33043e79fe40
ihasidul <ihasidul@gmail.com>
2022-01-11 11:29:51 +0600

    Add is this a triangle problems solution

---
+++ codewars/is_this_a_triangle.py
@@ -0,0 +1,7 @@
+def is_triangle(a, b, c):
+    if (a <= 0) or (b <= 0) or (c <= 0):
+        return False
+    elif (a + b > c) and (a + c > b) and (b + c > a):
+        return True
+    else:
+        return False
