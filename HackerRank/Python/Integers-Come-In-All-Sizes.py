62b360962372527b511dfa346facbae813707049
ihasidul <ihasidul@gmail.com>
2024-03-16 14:13:31 +0600

    Add new hackerrank folder for python related prblems

---
+++ HackerRank/Python/Integers-Come-In-All-Sizes.py
@@ -0,0 +1,12 @@
+"""
+Problem Statement:
+URL: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+result = a**b + c**d
+print(result)
