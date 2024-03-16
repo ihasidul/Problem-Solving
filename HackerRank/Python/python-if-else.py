69b879788c7834ca7531a198938d41640e5d741b
ihasidul <ihasidul@gmail.com>
2024-03-16 14:23:24 +0600

    Add new solution from hjackerranck

---
+++ HackerRank/Python/python-if-else.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+"""
+URL: https://www.hackerrank.com/challenges/py-if-else/problem
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if ((n % 2) != 0):
+        print("Weird")
+    elif ((n % 2) == 0) and n > 2 and n < 5 :
+        print("Not Weird")
+    elif ((n % 2) == 0) and n > 6 and n <= 20 :
+        print("Weird")
+    elif ((n % 2) == 0) and n > 20 :
+        print("Not Weird")
+
+    
+
